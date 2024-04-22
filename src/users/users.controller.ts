a47f8296050906be69d7785b9014803d81f86166
Raj <mukesh@Laptop334.local>
2024-04-22 21:17:37 +0530

    user crud

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,17 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Param,
+  Query,
+  Delete,
+  NotFoundException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -9,5 +20,29 @@ export class UsersController {
   @Post('signup')
   async createUser(@Body() user: CreateUserDto): Promise<any> {
     this.usersService.create(user);
+  }
+
+  @Get(':id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOne(parseInt(id));
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return user;
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.find(email);
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() user: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), user);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
   }
 }
