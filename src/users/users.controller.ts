98e075dc6a117acd2a5117850b69d34fb49c2090
Raj <mukesh@Laptop334.local>
2024-04-30 15:59:12 +0530

    auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,22 +8,56 @@ import {
   Query,
   Delete,
   NotFoundException,
-  UseInterceptors,
+  Session,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+import { CurrentUser } from './decorators/current-user.decorator';
 
 @Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly usersService: UsersService,
+  ) {}
 
   @Post('signup')
-  async createUser(@Body() user: CreateUserDto): Promise<any> {
-    this.usersService.create(user);
+  async createUser(
+    @Body() user: CreateUserDto,
+    @Session() session: any,
+  ): Promise<any> {
+    const userRes = await this.authService.signup(user?.email, user?.password);
+    session.userId = userRes.id;
+    return userRes;
+  }
+
+  @Get('whoami')
+  whoami(@CurrentUser() user: any) {
+    // return this.usersService.findOne(session.userId);
+    return user;
+  }
+
+  @Post('signin')
+  async signin(
+    @Body() user: CreateUserDto,
+    @Session() session: any,
+  ): Promise<any> {
+    const userRes = await this.authService.signin(user?.email, user?.password);
+    session.userId = userRes.id;
+    return userRes;
+  }
+
+  @Post('signout')
+  async signout(@Session() session: any): Promise<any> {
+    session.userId = null;
+    return {
+      message: 'Signout success',
+    };
   }
 
   @Get(':id')
@@ -49,4 +83,15 @@ export class UsersController {
   deleteUser(@Param('id') id: string) {
     return this.usersService.remove(parseInt(id));
   }
+
+  // @Get('colors/:color')
+  // async setColor(@Param('color') color: string, @Session() session: any) {
+  //   session.color = color;
+  // }
+
+  // @Get('colors')
+  // async getColor(@Session() session: any) {
+  //   console.log("session is", session);
+  //   return session.color;
+  // }
 }
